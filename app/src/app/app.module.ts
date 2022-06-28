d4ce5f4e267e8856c79be04184e6ab6986a78df8
atienoobwanda <atienoobwanda@gmail.com>
2022-06-28 13:51:28 +0300

    adding application routing

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { SignupUpPageComponent } from './components/signup-up-page/signup-up-page.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+
+
+const routes: Routes = [
+  {path:"", redirectTo:"login", pathMatch:"full"},
+  {path:"login", component:LoginPageComponent},
+  {path:"signUp", component:SignupUpPageComponent},
+  {path:"dashboard", component:DashboardComponent},
+];
+
 
 @NgModule({
   declarations: [
@@ -15,10 +29,15 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     DashboardComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    RouterModule
+    // RouterModule.forRoot(
+    //   routes,
+    //   { enableTracing: true }
+    // )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
