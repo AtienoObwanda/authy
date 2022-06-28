a69675a733f56a5bfcced019eb4bbd31e698a346
atienoobwanda <atienoobwanda@gmail.com>
2022-06-28 14:04:07 +0300

    adding a sign up form

--- app/src/app/components/signup-up-page/signup-up-page.component.ts
+++ app/src/app/components/signup-up-page/signup-up-page.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+
 @Component({
   selector: 'app-signup-up-page',
   templateUrl: './signup-up-page.component.html',
