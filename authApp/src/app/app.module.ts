45ea4cbc77020c2fb8f6fed070692b81ff5589dd
atienoobwanda <atienoobwanda@gmail.com>
2022-06-29 08:44:46 +0300

    adding global set up

--- authApp/src/app/app.module.ts
+++ authApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MdbCheckboxModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
