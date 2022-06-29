b4b69f0c62b3bd182f2fc68768e0191805a09a1a
atienoobwanda <atienoobwanda@gmail.com>
2022-06-29 06:57:31 +0300

    updating routes

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+import { SignupUpPageComponent } from './components/signup-up-page/signup-up-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+
+const routes: Routes = [
+  // {path:"", redirectTo:"login", pathMatch:"full"},
+  {path:"", component:LoginPageComponent},
+  {path:"login", component:LoginPageComponent},
+  {path:"signUp", component:SignupUpPageComponent},
+  {path:"dashboard", component:DashboardComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,24 +1,16 @@
 import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
-import { Routes } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+// import { Routes } from '@angular/router';
 import { RouterModule } from '@angular/router';
 
 
-// import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { SignupUpPageComponent } from './components/signup-up-page/signup-up-page.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-
-
-const routes: Routes = [
-  {path:"", redirectTo:"login", pathMatch:"full"},
-  {path:"login", component:LoginPageComponent},
-  {path:"signUp", component:SignupUpPageComponent},
-  {path:"dashboard", component:DashboardComponent},
-];
 
 
 @NgModule({
@@ -29,11 +21,10 @@ const routes: Routes = [
     DashboardComponent
   ],
   imports: [
+    BrowserModule,
+    AppRoutingModule,
     RouterModule
-    // RouterModule.forRoot(
-    //   routes,
-    //   { enableTracing: true }
-    // )
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
